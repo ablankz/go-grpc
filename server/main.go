593608c340bb02c1df3b8d5a04ac8d2706b4ce46
blank <hayaken3007@i.softbank.jp>
2023-12-17 17:12:09 +0900

    fileサービスのserver 側実装

--- server/main.go
+++ server/main.go
@@ -4,7 +4,11 @@ import (
 	"context"
 	"fmt"
 	"go-grpc/pb"
+	"log"
+	"net"
 	"os"
+
+	"google.golang.org/grpc"
 )
 
 type server struct {
@@ -32,4 +36,19 @@ func (*server) ListFiles(ctx context.Context, req *pb.ListFilesRequest) (*pb.Lis
 		Filenames: filenames,
 	}
 	return res, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", "localhost:50051")
+	if err != nil {
+		log.Fatalf("Failed to Listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterFileServiceServer(s, &server{})
+
+	fmt.Println("server is running")
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
 }
