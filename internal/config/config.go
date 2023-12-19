2c5d6713f2e921a4b75dddb7d34574cda31073dc
blank <hayaken3007@i.softbank.jp>
2023-12-19 23:51:50 +0900

    config作成

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,38 +9,10 @@ import (
 
 // Config アプリケーション設定を表す構造体。
 type Config struct {
-	// Port ポート
-	// 0 が指定された場合は動的にポートを割り当てる。
-	Port uint16 `env:"PORT" envDefault:"8080"`
-
-	// DBHost データベースホスト
-	DBHost string `env:"DB_HOST" envDefault:"localhost"`
-	// DBPort データベースポート
-	DBPort uint16 `env:"DB_PORT" envDefault:"3306"`
-	// DBName データベース名
-	DBName string `env:"DB_NAME,required"`
-	// DBUsername データベースユーザー名
-	DBUsername string `env:"DB_USERNAME,required"`
-	// DBPassword データベースパスワード
-	DBPassword string `env:"DB_PASSWORD"`
-
-	// AuthSecret 認証トークンの署名用シークレット
-	AuthSecret string `env:"AUTH_SECRET" envDefault:"secret"`
-
-	// AdminToolPath 管理ツールのローカルファイルシステム上のパス
-	AdminToolLocalPath string `env:"ADMIN_TOOL_LOCAL_PATH"`
-	// DocsLocalPath API ドキュメントのローカルファイルシステム上のパス
-	DocsLocalPath string `env:"DOCS_LOCAL_PATH"`
-
-	// ClientOrigin クライアントのオリジン
-	ClientOrigin string `env:"CLIENT_ORIGIN"`
-
-	// DebugCORS CORS デバッグモード
-	DebugCORS bool `env:"DEBUG_CORS"`
-	// FakeTime 時刻偽装モード設定
-	// 時刻が指定された場合はその時刻に固定する。
-	// Truthy な値が指定された場合はデフォルトの時刻に固定する。
-	FakeTime FakeTimeMode `env:"FAKE_TIME"`
+	Port      int    `env:"PORT" envDefault:"50051"`
+	Env       string `env:"GO_ENV" envDefault:"development"`
+	LocalRoot string `env:"LOCAL_ROOT_PATH,required"`
+	Debug     bool   `env:"DEBUG"`
 }
 
 var parseFuncMap = map[reflect.Type]env.ParserFunc{
